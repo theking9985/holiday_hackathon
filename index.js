e7b629d37fe586efefde6fa5f0575a99d7b7e517
theking9985 <theking9985@yahoo.com>
2015-12-08 13:42:16 -0800

    add procfile

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var bodyParser = require('body-parser');
 // More info: https://docs.nodejitsu.com/articles/file-system/how-to-use-the-path-module
 var path = require('path');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Mongoose stuff
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/holidayDB');
+// mongoose.connect('mongodb://localhost/holidayDB');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/holidayDB');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -31,11 +33,8 @@ app.get('/', function(req, res) {
   res.send(xmas);
 });
 
-
-// app.get('/', function(req, res) {
-//   res.send('Hi!');
-// });
-
-
-app.listen(3000);
+// app.listen(3000);
+app.listen(port, function() {
+    console.log("Port " + port);
+});
 
